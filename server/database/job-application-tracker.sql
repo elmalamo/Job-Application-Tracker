f1c794dfd38be417804cd34366322e80a2c29730
elmalamo <malamask10@hotmail.com>
2026-03-13 21:08:53 +0200

    fix: update the sql file for db creation

--- server/database/job-application-tracker.sql
+++ server/database/job-application-tracker.sql
@@ -1,19 +1,35 @@
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    password VARCHAR(250) NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TYPE application_status AS ENUM (
+    'Applied',
+    'Interview',
+    'Offer',
+    'Rejected'
+);
+
+CREATE TYPE work_mode AS ENUM ('onsite', 'remote', 'hybrid');
+
 CREATE TABLE applications (
-    id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id INTEGER NOT NULL,
     company VARCHAR(100) NOT NULL,
     position VARCHAR(100) NOT NULL,
-    status VARCHAR(50) NOT NULL,
+    location VARCHAR(30),
+    work_mode work_mode NOT NULL,
+    status application_status NOT NULL,
     notes TEXT,
     applied_at DATE NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_user
+        FOREIGN KEY (user_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE
 );
