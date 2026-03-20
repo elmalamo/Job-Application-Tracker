70b5e5677f687fa0e75bcd9013dbef3aa62f527e
elmalamo <malamask10@hotmail.com>
2026-03-20 15:10:06 +0200

    fix: delete some console logs

--- client/src/auth/ProtectedRoute.jsx
+++ client/src/auth/ProtectedRoute.jsx
@@ -5,18 +5,15 @@ const ProtectedRoute = ({ redirectPath = "/login" }) => {
   const { user, loading } = useAuth();
 
   if (loading) {
-    console.log('BLOCKING - loading:', loading, 'isLoggingOut:', isLoggingOut);
     return null;
   }
 
   //if user doesnt exist redirect to login page
   if (!user) {
-    console.log('NO USER - redirecting to login')
     return <Navigate to={redirectPath} replace />;
   }
 
   //if user exists render children 
-  console.log('USER EXISTS - rendering outlet')
   return <Outlet />;
 };
 
