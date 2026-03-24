f62caba130dc8fdacbc84733f84a2e6ab53ac4ac
elmalamo <malamask10@hotmail.com>
2026-03-24 12:43:28 +0200

    fix: change app.js so it doenst respond 500 when i login

--- server/app.js
+++ server/app.js
@@ -5,8 +5,13 @@ import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import applicationsRoutes from "./routes/applications.routes.js";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config({ path: join(__dirname, ".env") });
 
 const app = express();
 
