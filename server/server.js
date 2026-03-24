edc1ac8ee3dfed0fef9fc8d23b11a16291085228
elmalamo <malamask10@hotmail.com>
2026-03-24 12:35:33 +0200

    fix: install package and change server.js so i can run the app using one command

--- server/server.js
+++ server/server.js
@@ -1,4 +1,12 @@
 import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config({ path: join(__dirname, ".env") });
+
 import app from "./app.js";
 
 dotenv.config();
