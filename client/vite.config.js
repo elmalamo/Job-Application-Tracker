d013c21c54b0b899ab43c7d8e917105898f13b33
elmalamo <malamask10@hotmail.com>
2026-03-25 21:43:12 +0200

    fix: edit vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,29 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    dedupe: ['@emotion/react', '@emotion/styled', '@mui/material'],
+    dedupe: ["@emotion/react", "@emotion/styled", "@mui/material"],
   },
   optimizeDeps: {
     include: [
-      '@emotion/react',
-      '@emotion/styled',
-      '@mui/material',
-      '@mui/x-date-pickers',
+      "@emotion/react",
+      "@emotion/styled",
+      "@mui/material",
+      "@mui/x-date-pickers",
     ],
   },
-    build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-          'mui-core': ['@mui/material', '@emotion/react', '@emotion/styled'],
-          'mui-icons': ['@mui/icons-material'],
-          'mui-pickers': ['@mui/x-date-pickers'],
-        }
-      }
-    }
-  },
-})
+});
