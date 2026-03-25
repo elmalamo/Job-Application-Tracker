14e64712ba99a97f44b4614e997cdc50d9d42ca5
elmalamo <malamask10@hotmail.com>
2026-03-25 22:55:59 +0200

    fix: add db url for production

--- server/database/db.js
+++ server/database/db.js
@@ -1,15 +1,24 @@
 import pg from "pg";
 import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-const pool = new pg.Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false
-});
+dotenv.config({ path: join(__dirname, ".env") });
+
+const pool = process.env.DB_URL
+  ? new pg.Pool({
+      connectionString: process.env.DB_URL,
+      ssl: { rejectUnauthorized: false },
+    })
+  : new pg.Pool({
+      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT,
+    });
 
 export default pool;
