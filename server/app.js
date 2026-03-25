b4ea74543125ddcbf30c13ba77d4e47a80566220
elmalamo <malamask10@hotmail.com>
2026-03-25 21:50:37 +0200

    fix: update app.js , change the client url

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: `http://localhost:${process.env.CLIENT_PORT}`,
+    origin: process.env.CLIENT_URL || `http://localhost:${process.env.CLIENT_PORT}`,
     credentials: true,
   }),
 );
